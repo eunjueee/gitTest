54dcb21d66b583ff9eefedd38e1d7df62aac7bdd
eunjueee <eunjueee@naver.com>
2021-04-12 09:28:24 +0900

    version1 branch MainJava test commit

---
+++ src/main/java/javaMain.java
@@ -0,0 +1,13 @@
+/**
+ * @author eunjueee
+ * @Class PACKAGE_NAME.javaMain
+ * @Description
+ * @since 2021-04-12
+ */
+
+public class javaMain {
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
